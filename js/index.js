34ed1ad2fb43136a62a9e37bdd574e0a070529b4
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-19 16:29:08 -0800

    main deliverables achieved

--- js/index.js
+++ js/index.js
@@ -1,1 +1,106 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+const bookURL = 'http://localhost:3000/books'
+const listPanel = document.getElementById('list-panel')
+const showPanel = document.getElementById('show-panel')
+let allBooks = []
+const me = {id: 11, username: "zoaesis"}
+
+getAllBooks();
+
+//DATA
+// Get
+
+function getAllBooks() {
+  fetch(bookURL)
+  .then(res => res.json())
+  .then(books => {
+    allBooks = books
+    populateList()
+  })
+}
+
+function getOneBook(id) {
+  fetch(bookURL+`/${id}`)
+  .then(res => res.json())
+  .then(book => {
+    showBook(book)
+  })
+}
+
+// Patch
+
+function likeBook(book) {
+  const addUser = [...book.users, me]
+
+  fetch(bookURL+`/${book.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "users": addUser
+    })
+  })
+  .then(res => res.json())
+  .then(book => {
+    showBook(book)
+  })
+  .catch(error => console.log(error.message))
+}
+
+//DOM 
+
+function populateList() {
+  allBooks.forEach(book => {
+    createBook(book)
+  })
+}
+
+function createBook(bookObject) {
+  const li = document.createElement('li')
+  li.textContent = bookObject.title
+  li.id = bookObject.id
+  li.addEventListener('click', handleClick)
+  listPanel.appendChild(li)
+}
+
+function showBook(book) {
+  showPanel.innerHTML = ""
+
+  let container = document.createElement('div')
+  let img = document.createElement('img')
+  let title = document.createElement('h3')
+  let subTitle = document.createElement('h3')
+  let author = document.createElement('h3')
+  let description = document.createElement('p')
+  let likers = document.createElement('ul')
+  let likeBtn = document.createElement('button')
+
+  container.className = "container"
+  img.src = book.img_url
+  title.textContent = book.title
+  if(book.subtitle){
+    subTitle.textContent = book.subtitle
+  }
+  author.textContent = book.author
+  description.textContent = book.description
+  book.users.forEach(user => {
+    let li = document.createElement('li')
+    li.textContent = user.username
+    likers.appendChild(li)
+  })
+  likeBtn.textContent = "❤️"
+  likeBtn.addEventListener("click", () => handleLike(book))
+
+  container.append(img, title, subTitle, author, description, likers, likeBtn)
+  showPanel.appendChild(container)
+}
+
+// HANDLERS
+function handleClick(e) {
+  getOneBook(e.target.id)
+}
+
+function handleLike(book) {
+  likeBook(book)
+}
